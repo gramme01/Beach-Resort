5e46941bf5663600cc1cb32079a79fd9434d7311
Emmanuel <gramme.oe@gmail.com>
2019-05-31 22:29:35 +0100

    Services Section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
+import Services from "../components/Services";
 
 const Home = () => {
 	return (
-		<Hero>
-			<Banner
-				title='luxurious rooms'
-				subtitle='deluxe rooms statrting at $299'
-			>
-				<Link to='/rooms' className='btn-primary'>
-					our rooms
-				</Link>
-			</Banner>
-		</Hero>
+		<>
+			<Hero>
+				<Banner
+					title='luxurious rooms'
+					subtitle='deluxe rooms statrting at $299'
+				>
+					<Link to='/rooms' className='btn-primary'>
+						our rooms
+					</Link>
+				</Banner>
+			</Hero>
+			<Services />
+		</>
 	);
 };
 
