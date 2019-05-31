68432aa8f660f4a01617382742ff989cffe84fa2
Emmanuel <gramme.oe@gmail.com>
2019-05-31 20:58:58 +0100

    Banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
 
 const Home = () => {
-	return <Hero />;
+	return (
+		<Hero>
+			<Banner
+				title='luxurious rooms'
+				subtitle='deluxe rooms statrting at $299'
+			>
+				<Link to='/rooms' className='btn-primary'>
+					our rooms
+				</Link>
+			</Banner>
+		</Hero>
+	);
 };
 
 export default Home;
