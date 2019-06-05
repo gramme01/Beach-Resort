82377b6319aa280f2ec781ac5d99ed540a548fb7
Emmanuel <gramme.oe@gmail.com>
2019-06-05 01:15:24 +0100

    Updates

---
+++ precache-manifest.27648adf15a0a9afa481fe00c2981cc4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f5ef5816573976a9cf6d311d3bb731",
+    "url": "/beach-resort/index.html"
+  },
+  {
+    "revision": "05e654b2bf238a3da36f",
+    "url": "/beach-resort/static/css/main.e7e7a86a.chunk.css"
+  },
+  {
+    "revision": "a3f273b8aa803fe635ab",
+    "url": "/beach-resort/static/js/2.23faa51b.chunk.js"
+  },
+  {
+    "revision": "05e654b2bf238a3da36f",
+    "url": "/beach-resort/static/js/main.99df15de.chunk.js"
+  },
+  {
+    "revision": "031eaf1266f036f82d54",
+    "url": "/beach-resort/static/js/runtime~main.ff43087a.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/beach-resort/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/beach-resort/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/beach-resort/static/media/logo.9f0cccc1.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/beach-resort/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/beach-resort/static/media/room-2.052c7681.jpeg"
+  }
+]);
